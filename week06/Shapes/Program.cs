369f04962cd3c5f0d5791b3ed05ba425c432a527
sebasss18 <sebasbernals11@gmail.com>
2025-12-02 14:51:33 -0700

    Shapes activity

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        Console.WriteLine("\nHello World! This is the Shapes Project.\n");
+
+        Square s = new Square("red", 5);
+
+        Console.WriteLine();
+        Console.WriteLine(s.GetColor());
+        Console.WriteLine(s.GetArea());
+
+        Rectangle r = new Rectangle("blue", 6, 9);
+
+        Console.WriteLine();
+        Console.WriteLine(r.GetColor());
+        Console.WriteLine(r.GetArea());
+
+        Circle c = new Circle("yellow", 8);
+
+        Console.WriteLine();
+        Console.WriteLine(c.GetColor());
+        Console.WriteLine(c.GetArea());
     }
 }
